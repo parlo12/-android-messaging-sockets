35ede55aac94266f43f2c1d98ccd6c7d5e478800
Benjamini Buganzi <benjamineluboya@gmail.com>
2024-12-21 01:41:43 +0300

    migration to new API

--- app/src/main/java/com/rhm/rhmsmsgateway/services/RetrofitInstance.kt
+++ app/src/main/java/com/rhm/rhmsmsgateway/services/RetrofitInstance.kt
@@ -4,8 +4,8 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitInstance {
-    // private const val BASE_URL = "https://coral-app-cazak.ondigitalocean.app"
-    private const val BASE_URL = "http://10.0.2.2:3000"
+    private const val BASE_URL = "https://coral-app-cazak.ondigitalocean.app"
+    // private const val BASE_URL = "http://10.0.2.2:3000"
 
     val api: ApiService by lazy {
         Retrofit.Builder()
--- app/src/main/java/com/rhm/rhmsmsgateway/services/SmsForegroundService.kt
+++ app/src/main/java/com/rhm/rhmsmsgateway/services/SmsForegroundService.kt
@@ -54,8 +54,8 @@ class SmsForegroundService : Service() {
             // Pass the token as a query parameter
             val options = IO.Options()
             options.query = "token=$token&deviceId=$mId" // Attach the token
-            // socket = IO.socket("ws://coral-app-cazak.ondigitalocean.app", options) // Replace with your server URL
-            socket = IO.socket("http://10.0.2.2:3000", options) // Replace with your server URL
+            socket = IO.socket("ws://coral-app-cazak.ondigitalocean.app", options) // Replace with your server URL
+            // socket = IO.socket("http://10.0.2.2:3000", options) // Replace with your server URL
             socket.connect()
             val mainChannel = "sendSMS"
             socket.on(mainChannel) { args ->
